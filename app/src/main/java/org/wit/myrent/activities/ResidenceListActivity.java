59b01891d3b2efb71167d46a15d878c1009661a4
John Kennedy <johnwjken@gmail.com>
2016-09-23 12:19:00 +0100

    Include Intents in ResidenceListActivity

--- app/src/main/java/org/wit/myrent/activities/ResidenceListActivity.java
+++ app/src/main/java/org/wit/myrent/activities/ResidenceListActivity.java
@@ -2,10 +2,12 @@ package org.wit.myrent.activities;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.ListView;
@@ -18,7 +20,7 @@ import org.wit.myrent.models.Residence;
 
 import java.util.ArrayList;
 
-public class ResidenceListActivity extends Activity {
+public class ResidenceListActivity extends Activity implements AdapterView.OnItemClickListener{
     private ListView listView;
     private Portfolio portfolio;
     private ResidenceAdapter adapter;
@@ -37,9 +39,16 @@ public class ResidenceListActivity extends Activity {
 
         adapter = new ResidenceAdapter(this, portfolio.residences);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(this);
     }
 
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(this, ResidenceActivity.class);
+        startActivity(intent);
+    }
 }
 class ResidenceAdapter extends ArrayAdapter<Residence> {
     private Context context;
