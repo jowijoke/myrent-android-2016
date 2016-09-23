c009949e6482ce148d4c01bbe02c29cf75b6c07d
John Kennedy <johnwjken@gmail.com>
2016-09-23 12:59:04 +0100

    Included LogHelper

--- app/src/main/java/org/wit/myrent/app/MyRentApp.java
+++ app/src/main/java/org/wit/myrent/app/MyRentApp.java
@@ -5,6 +5,8 @@ import android.app.Application;
 
 import org.wit.myrent.models.Portfolio;
 
+import static org.wit.android.helpers.LogHelpers.info;
+
 public class MyRentApp extends Application {
     public Portfolio portfolio;
 
@@ -13,5 +15,7 @@ public class MyRentApp extends Application {
     {
         super.onCreate();
         portfolio = new Portfolio();
+
+        info(this, "MyRent app launched");
     }
 }
