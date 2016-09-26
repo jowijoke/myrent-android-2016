042a49d76d904ee9a8a3014f9d1a9c5a0e8753e2
John Kennedy <johnwjken@gmail.com>
2016-09-26 12:14:51 +0100

    Update portfolio model to save, restore Residences using PortfolioSerializer

--- app/src/main/java/org/wit/myrent/models/Portfolio.java
+++ app/src/main/java/org/wit/myrent/models/Portfolio.java
@@ -4,11 +4,31 @@ import android.util.Log;
 
 import java.util.ArrayList;
 
+import static org.wit.android.helpers.LogHelpers.info;
+
 public class Portfolio {
     public ArrayList<Residence> residences;
+    private PortfolioSerializer serializer;
 
-    public Portfolio() {
-        residences = new ArrayList<Residence>();
+    public Portfolio(PortfolioSerializer serializer) {
+        this.serializer = serializer;
+        try {
+            residences = serializer.loadResidences();
+        } catch (Exception e) {
+            info(this, "Error loading residences: " + e.getMessage());
+            residences = new ArrayList<Residence>();
+        }
+    }
+
+    public boolean saveResidences() {
+        try {
+            serializer.saveResidences(residences);
+            info(this, "Residences saved to file");
+            return true;
+        } catch (Exception e) {
+            info(this, "Error saving residences: " + e.getMessage());
+            return false;
+        }
     }
 
     public void addResidence(Residence residence) {
@@ -23,7 +43,13 @@ public class Portfolio {
                 return res;
             }
         }
+        info(this, "failed to find residence. returning first element array to avoid crash");
         return null;
+    }
+
+    public void deleteResidence(Residence residence) {
+        residences.remove(residence);
+        saveResidences();
     }
 
 }
